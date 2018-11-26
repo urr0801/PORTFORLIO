1e8fb4f767ea9b3d8fb06694c27be20470e6c63b
booldook <30773555+booldook@users.noreply.github.com>
2018-11-26 15:23:21 +0900

    booldook

--- public/js/portfolio.js
+++ public/js/portfolio.js
@@ -51,6 +51,9 @@ function homeAdd(data) {
 	html += '</div>';
 	$("#port_wrap").append(html);
 	$(window).trigger("resize");
+	$(".site_img").eq(0).addClass("ani_img");
+	$(".site_txt").eq(0).addClass("ani_txt");
+
 }
 function homeRev(data) {
 	var id = data.key;
@@ -76,6 +79,7 @@ $(window).resize(function(){
 		gap[i] = $(this).offset().top;
 	});
 }).trigger("resize");
+
 $(window).on("mousewheel DOMMouseScroll", wheelFn);
 function wheelFn(e) {
 	e.preventDefault();
@@ -90,18 +94,46 @@ function wheelFn(e) {
 		}
 	}
 	if (dir > 0) { if (now > 0) now--; } 
-	else { if (now < gap.length - 1) now++; }
-		$(".site_img").eq(0).addClass("ani_img");
-		$(".site_txt").eq(0).addClass("ani_txt");
-		$("html, body").stop().animate({"scrollTop": gap[now] + "px"}, 200, function(){
+	else { 
+		if (now < gap.length - 1) now++; 
+	}
+	
+	$("html, body").stop().animate({"scrollTop": gap[now] + "px"}, 200, function(){
 		$(window).on("mousewheel DOMMouseScroll", wheelFn);
-		
 		$(".site_img").eq(now).addClass("ani_img");
 		$(".site_txt").eq(now).addClass("ani_txt");
 		console.log(now);
 	});
-	}
-
+}
 
 // 높이에따른 애니메이션
 
+function includeHTML() {
+  var z, i, elmnt, file, xhttp;
+  /*loop through a collection of all HTML elements:*/
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("w3-include-html");
+    if (file) {
+      /*make an HTTP request using the attribute value as the file name:*/
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+          /*remove the attribute, and call this function once more:*/
+          elmnt.removeAttribute("w3-include-html");
+          includeHTML();
+        }
+      } 
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /*exit the function:*/
+      return;
+    }
+  }
+}
+
+includeHTML();
