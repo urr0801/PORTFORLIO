fe539c18004bf807878f575a4687289e318eefdb
urr0801 <urr0801@gmail.com>
2018-11-28 17:59:41 +0900

    #009

--- public/js/portfolio.js
+++ public/js/portfolio.js
@@ -37,7 +37,7 @@ function homeAdd(data) {
 	var html = '';
 	html += '<div class="site_bg page" id="'+id+'">';
 	html += '<div class="site_box row">';
-	html += '<div class="site_img"><img src="'+src+'" alt="site_img" class="img mov_img"></div>';
+	html += '<div class="site_img"><img src="'+src+'" alt="site_img" class="img"></div>';
 	html += '<ul class="site_txt">';
 	html += '<li><h3>project Name</h3></li>';
 	html += '<li><h2>'+adtitle+'</h2></li>';
@@ -54,9 +54,15 @@ function homeAdd(data) {
 	html += '</div>';
 	$("#port_wrap").append(html);
 	$(window).trigger("resize");
-	$(".site_img").eq(0).addClass("ani_img");
-	$(".site_txt").eq(0).addClass("ani_txt");
-	$(".but").eq(0).addClass("ani_but");
+	$(".site_img").eq(0).addClass("ani_img0");
+	$(".site_txt").eq(0).addClass("ani_txt0");
+	$(".but").eq(0).addClass("ani_but0");
+	// img scale ani
+	$(".site_img").hover(function(){
+		$(this).find("img").css({"animation-name":"siteAni"});
+	}, function(){
+		$(this).find("img").css({"animation-name":"siteAniBack"});
+	});
 }
 function homeRev(data) {
 	var id = data.key;
@@ -103,23 +109,19 @@ function wheelFn(e) {
 	$("html, body").stop().animate({"scrollTop": gap[now] + "px"}, 200, function(){
 		$(window).on("mousewheel DOMMouseScroll", wheelFn);
 		// page ani event
-		$(".site_img").eq(now).addClass("ani_img");
-		$(".site_txt").eq(now).addClass("ani_txt");
-		$(".but").eq(now).addClass("ani_but");
+		$(".site_img").eq(now).addClass("ani_img"+now);
+		$(".site_txt").eq(now).addClass("ani_txt"+now);
+		$(".but").eq(now).addClass("ani_but"+now);
 	});
 }
 
+// img 스케일 애니메이션
+// $(".site_img").hover(function(){
+// 	$(this).find("img").css({"animation-name":"siteAni"});
+// }, function(){
+// 	$(this).find("img").css({"animation-name":"siteAniBack"});
+// });
 
-$(".site_img").mousemove(function(evt){
-	var delta = 50;
-	var cX = evt.clientX;
-	var cY = evt.clientY;
-	var iX = $(this).find(".mov_img").width()/2;
-	var iY = $(this).find(".mov_img").height()/2;
-	var mX = (iX - cX)/delta;
-	var mY = (iY - cY)/delta;
-	$(this).find(".mov_img").css("transform", "translate("+mX+"px, "+mY+"px)");
-});
 
 // function includeHTML() {
 //   var z, i, elmnt, file, xhttp;
